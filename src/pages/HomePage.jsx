965a7cd7f94174d9dc6eadb75f48d80808446387
elmirshidev <rzazadeelmir19@gmail.com>
2023-09-13 21:40:45 +0400

    framer1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ function HomePage() {
 
   return (
     <div className='flex justify-center items-center pt-[150px]'>
-      <motion.h1 
-       className='text-gray-950 text-7xl font-[800] flex 500:text-5xl'>TechAnt <GiAnt /></motion.h1>
+      <h1 
+       className='text-gray-950 text-7xl font-[800] flex 500:text-5xl'>TechAnt <GiAnt /></h1>
     </div>
   )
 }
